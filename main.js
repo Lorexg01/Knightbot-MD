9270e69cc1e98898dccaafa3347c12b863bbd67d
Lorexg01 <160356054+Lorexg01@users.noreply.github.com>
2025-04-19 11:44:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -73,17 +73,17 @@ const { autoStatusCommand, handleStatusUpdate } = require('./commands/autostatus
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://whatsapp.com/channel/0029VataDSsLCoWuKM1j2D0i";
+global.ytch = "Mr Lorex";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
     contextInfo: {
-        forwardingScore: 999,
+        forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'MrBeast MD',
             serverMessageId: -1
         }
     }
